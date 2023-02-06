1756c1649f85129716df6d9fd95fd978dafe0d59
Arthurchkeee <arthurchkeee@gmail.com>
2023-02-06 19:41:24 +0300

    Initial commit

---
+++ constants.js
@@ -0,0 +1,29 @@
+const COLS = 10;
+const ROWS = 20;
+const BLOCK_SIZE = 30;
+
+const KEY = ['ArrowLeft', 'ArrowRight', 'ArrowDown', 'Space', 'ArrowUp'];
+const COLORS=[
+    'blue',
+    'red',
+    'green',
+    'yellow',
+    'purple',
+    'pink',
+    'brown'
+];
+const SHAPES = [[[0, 1, 1], [1, 1, 0], [0, 0, 0]],
+    [[2, 2], [2, 2]],
+    [[3,3,0],[0,3,3],[0,0,0]],
+    [[0,0,4],[4,4,4],[0,0,0]],
+    [[5,0,0],[5,5,5],[0,0,0]],
+    [[0,6,0],[6,6,6],[0,0,0]],
+    [[0,0,0,0],[7,7,7,7],[0,0,0,0],[0,0,0,0]]
+];
+
+const POINTS={
+    ONE: 40,
+    TWO: 100,
+    THREE: 300,
+    FOUR: 1200
+}
