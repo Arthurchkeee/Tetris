7d33f800a20d2878e38786a47ef7081c4cdef530
Arthurchkeee <arthurchkeee@gmail.com>
2023-02-10 20:39:46 +0300

    add control on wasd buttons, add background music, add turn on left side refactor files between folders

---
+++ js/board.js
@@ -0,0 +1,151 @@
+class Board {
+    constructor(ctx, ctxNext) {
+        this.ctx = ctx;
+        this.ctxNext = ctxNext;
+        this.piece = null;
+        this.pieceNext = null;
+    }
+
+    getNewPiece() {
+        this.pieceNext = new Piece(this.ctxNext);
+        this.ctxNext.clearRect(0, 0, ctxNext.canvas.width, ctxNext.canvas.height);
+        this.pieceNext.draw();
+    }
+
+    reset() {
+        this.grid = this.getEmptyBoard();
+        this.piece = new Piece(this.ctx);
+        this.getNewPiece();
+    }
+
+    getEmptyBoard() {
+        return Array.from({length: ROWS}, () => Array(COLS).fill(0));
+    }
+
+    insideWalls(x) {
+        return x >= 0 && x < COLS;
+    }
+
+    underFloor(y) {
+        return y <= ROWS;
+    }
+
+    notOccupied(x, y) {
+        return this.grid[y] && this.grid[y][x] === 0;
+    }
+
+    valid(p) {
+        return p.shape.every((row, dy) => {
+            return row.every((value, dx) => {
+                let x = p.x + dx;
+                let y = p.y + dy;
+                return value === 0 ||
+                    (this.insideWalls(x) && this.underFloor(y) && this.notOccupied(x, y));
+            });
+        });
+    }
+
+    rightRotate(p) {
+        for (let y = 0; y < p.shape.length; ++y) {
+            for (let x = 0; x < y; ++x) {
+                [p.shape[x][y], p.shape[y][x]] =
+                    [p.shape[y][x], p.shape[x][y]];
+            }
+        }
+        p.shape.forEach(row => row.reverse());
+        return p;
+    }
+
+    leftRotate(p) {
+        for (let y = 0; y < p.shape.length; ++y) {
+            for (let x = 0; x < y; ++x) {
+                [p.shape[x][y], p.shape[y][x]] =
+                    [p.shape[y][x], p.shape[x][y]];
+            }
+        }
+        p.shape.reverse();
+        return p;
+    }
+
+    draw() {
+        this.piece.draw();
+        this.drawBoard();
+    }
+
+    drop() {
+        let p = {...board.piece};
+        p.y += 1;
+        if (this.valid(p)) {
+            this.piece.move(p);
+        } else {
+            this.freeze();
+            this.clearLines();
+            if (this.piece.y === 0) {
+                // Game over
+                return false;
+            }
+            this.piece = this.pieceNext;
+            this.piece.x = 3;
+            this.piece.y = 0;
+            this.piece.ctx = this.ctx;
+            this.getNewPiece();
+        }
+        return true;
+    }
+
+    freeze() {
+        this.piece.shape.forEach((row, y) => {
+            row.forEach((value, x) => {
+                if (value > 0) {
+                    this.grid[y + this.piece.y][x + this.piece.x] = value;
+                }
+            })
+        });
+    }
+
+    drawBoard() {
+        this.grid.forEach((row, y) => {
+            row.forEach((value, x) => {
+                if (value > 0) {
+                    this.ctx.fillStyle = COLORS[value - 1];
+                    this.ctx.fillRect(x, y, 1, 1);
+                }
+            })
+        });
+    }
+
+    clearLines() {
+        let lines = 0;
+        this.grid.forEach((row, y) => {
+            if (row.every(value => value > 0)) {
+                lines++;
+                this.grid.splice(y, 1);
+                this.grid.unshift(Array(COLS).fill(0));
+            }
+        });
+        if (lines > 0) {
+            data.score += this.getLineClearPoints(lines, data.level);
+            data.lines += lines;
+            if (data.lines > data.level * 10) {
+                time.level -= 100;
+                console.log(time.level)
+                data.level++;
+            }
+            updateData(data);
+        }
+    }
+
+    getLineClearPoints(lines, level) {
+        switch (lines) {
+            case 1:
+                return POINTS.ONE * level;
+            case 2:
+                return POINTS.TWO * level;
+            case 3:
+                return POINTS.THREE * level;
+            case 4:
+                return POINTS.FOUR * level;
+        }
+    }
+
+}
---
+++ js/main.js
@@ -0,0 +1,108 @@
+const canvas = document.getElementById('board');
+const ctx = canvas.getContext('2d');
+ctx.canvas.width = COLS * BLOCK_SIZE;
+ctx.canvas.height = ROWS * BLOCK_SIZE;
+ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
+
+const canvasNext = document.getElementById('next');
+const ctxNext = canvasNext.getContext('2d');
+ctxNext.canvas.width = 4 * BLOCK_SIZE;
+ctxNext.canvas.height = 4 * BLOCK_SIZE;
+ctxNext.scale(BLOCK_SIZE, BLOCK_SIZE);
+
+let board = new Board(ctx, ctxNext);
+
+let requestId;
+const time = {start: 0, elapsed: 0, level: 1000};
+let data = {score: 0, lines: 0, level: 1}
+
+function play() {
+    resetGame();
+    animate();
+}
+
+
+document.addEventListener('keydown', event => {
+    console.log(event.code);
+    event.preventDefault();
+    let p = {...board.piece};
+    if (KEY.LEFT.includes(event.code)) {
+        p.x -= 1;
+    } else if (KEY.RIGHT.includes(event.code)) {
+        p.x += 1;
+    } else if (KEY.DOWN.includes(event.code)) {
+        p.y += 1;
+    } else if (KEY.SPACE.includes(event.code)) {
+        p.y += 1;
+    } else if (KEY.TURN_RIGHT.includes(event.code)) {
+        board.rightRotate(p);
+    } else if(KEY.TURN_LET.includes(event.code)){
+        board.leftRotate(p);
+    }
+
+    if (KEY.SPACE.includes(event.code)) {
+        while (board.valid(p)) {
+            board.piece.move(p);
+            ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+            board.piece.draw();
+            p.y += 1;
+        }
+    } else if (board.valid(p)) {
+        board.piece.move(p);
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        board.piece.draw();
+    }
+
+
+});
+
+
+function animate(now = 0) {
+    time.elapsed = now - time.start;
+    if (time.elapsed > time.level) {
+        time.start = now;
+        if (!board.drop()) {
+            gameOver();
+            return;
+        }
+    }
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    board.draw();
+    requestAnimationFrame(animate);
+}
+
+function gameOver() {
+    cancelAnimationFrame(requestId);
+    ctx.fillStyle = 'cyan';
+    ctx.fillRect(1, 3, 8, 1.2);
+    ctx.font = '1px Comic Sans MS';
+    ctx.fillStyle = 'red';
+    ctx.fillText('GAME OVER', 1.8, 4);
+}
+
+function updateData(value) {
+    let score = document.getElementById('score');
+    if (score) {
+        score.textContent = value.score;
+    }
+    let lines = document.getElementById('lines');
+    if (lines) {
+        lines.textContent = value.lines;
+    }
+    let level = document.getElementById('level');
+    if (level) {
+        level.textContent = value.level;
+    }
+}
+
+function resetGame() {
+    data.level = 1;
+    data.lines = 0;
+    data.score = 0;
+    time.level = 1000;
+    board.reset();
+    board.piece = new Piece(ctx);
+    board.piece.x = 3;
+    board.piece.y = 0;
+
+}
